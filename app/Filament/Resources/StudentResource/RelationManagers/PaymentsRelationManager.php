aa9691e7971a70c01a8528d711ae1d6368a4db37
SayidMuhammad007 <uzbekcoder1507@gmail.com>
2024-12-13 20:48:38 +0500

    test

--- app/Filament/Resources/StudentResource/RelationManagers/PaymentsRelationManager.php
+++ app/Filament/Resources/StudentResource/RelationManagers/PaymentsRelationManager.php
@@ -80,10 +80,10 @@ class PaymentsRelationManager extends RelationManager
                 //
             ])
             ->headerActions([
-                Tables\Actions\CreateAction::make(),
+                // Tables\Actions\CreateAction::make(),
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                // Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
